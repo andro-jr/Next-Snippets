6f153d7cf818b782dbba2f8164f6ef11e581c02a
andro-jr <prabin20panta@gmail.com>
2024-06-29 16:03:38 +0545

    beautify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,32 @@
 import { db } from "@/db";
+import Link from "next/link";
 import React from "react";
 
 const Home = async () => {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => (
-    <div
-      key={snippet.id}
-      className="bg-black bg-opacity-70 rounded-lg p-5 m-4 shadow-lg"
+    <Link
+      href={`/snippets/${snippet.id}`}
+      className="flex text-xl mb-2 bg-black bg-opacity-40 rounded-lg p-5 shadow-lg"
     >
-      <div className="text-xl mb-2">{snippet.title}</div>
-    </div>
+      <div key={snippet.id} className="flex">
+        <div>{snippet.title}</div>
+      </div>
+    </Link>
   ));
 
   return (
-    <div className="min-h-screen flex items-center justify-center text-white">
+    <div className="min-h-screen flex flex-col items-center justify-center text-white">
+      <div className="container mx-auto text-center px-4 flex w-full justify-between items-center mb-6">
+        <h1 className="text-4xl font-bold text-white">Code Snippets</h1>
+        <div>
+          <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300">
+            New
+          </button>
+        </div>
+      </div>
+
       <div className="container mx-auto text-center px-4">
         {renderedSnippets}
       </div>
