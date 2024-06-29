8295322d76f47a27b33d18c6a0f63cb1c56dc06f
andro-jr <prabin20panta@gmail.com>
2024-06-29 15:17:43 +0545

    fetching particular records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,24 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const SnippetDetails = ({ params }: any) => {
-  return <div>{params.id} cha</div>;
+interface SnippetDetailsProp {
+  params: {
+    id: string;
+  };
+}
+
+const SnippetDetails = async ({ params }: SnippetDetailsProp) => {
+  const id = parseInt(params.id);
+
+  const snippet = await db.snippet.findFirst({
+    where: {
+      id,
+    },
+  });
+  if (!snippet) return notFound();
+
+  return <div>{snippet.title}</div>;
 };
 
 export default SnippetDetails;
