96bbe4be9e2f0b29cadbb3cfc6fd49ee944c7cba
andro-jr <prabin20panta@gmail.com>
2024-06-29 16:20:41 +0545

    styling loading

--- src/app/snippets/[id]/loading.tsx
+++ src/app/snippets/[id]/loading.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const SnippetLoading = () => {
-  return <div>Loading..</div>;
+const SnippetLoading: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col items-center">
+        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20 mb-4"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SnippetLoading;
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -51,7 +51,7 @@ const DeleteIcon = () => (
 const SnippetDetails: React.FC<SnippetDetailsProp> = async ({ params }) => {
   const id = parseInt(params.id);
 
-  await new Promise((r) => setTimeout(r, 5000));
+  await new Promise((r) => setTimeout(r, 2000));
 
   const snippet = await db.snippet.findFirst({
     where: {
