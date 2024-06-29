0653658afb5388713adfdea835cd58d71316a808
andro-jr <prabin20panta@gmail.com>
2024-06-29 15:03:58 +0545

    fetching data with serve component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { db } from "@/db";
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = async () => {
+  const snippets = await db.snippet.findMany();
+  console.log("snippets :", snippets);
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return <div>{renderedSnippets}</div>;
 };
 
 export default Home;
