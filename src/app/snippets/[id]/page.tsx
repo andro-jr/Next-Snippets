23887c7466276bd7552cb3ea32cb88e5c13a219e
andro-jr <prabin20panta@gmail.com>
2024-06-29 15:33:46 +0545

    loading

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -11,6 +11,8 @@ interface SnippetDetailsProp {
 const SnippetDetails = async ({ params }: SnippetDetailsProp) => {
   const id = parseInt(params.id);
 
+  await new Promise((r) => setTimeout(r, 5000));
+
   const snippet = await db.snippet.findFirst({
     where: {
       id,
