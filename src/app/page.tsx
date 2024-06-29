c94edbb161cd25b16e32d0b406b60f8400ebedbc
andro-jr <prabin20panta@gmail.com>
2024-06-29 15:10:39 +0545

    dynamic paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 const Home = async () => {
   const snippets = await db.snippet.findMany();
-  console.log("snippets :", snippets);
 
   const renderedSnippets = snippets.map((snippet) => {
     return <div key={snippet.id}>{snippet.title}</div>;
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SnippetDetails = ({ params }: any) => {
+  return <div>{params.id} cha</div>;
+};
+
+export default SnippetDetails;
